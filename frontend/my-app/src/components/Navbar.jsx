d0c8f7c9d5ea9bfb7f8d7486ab955b287a7ad7b2
Aayush-Kr-13 <2301aayush@gmail.com>
2024-08-22 19:52:19 +0530

    Stats Page Added and Navbar and Landing Page responsive

--- frontend/my-app/src/components/Navbar.jsx
+++ frontend/my-app/src/components/Navbar.jsx
@@ -5,14 +5,16 @@ import './Navbar.css';
 export default function Navbar() {
   return (
     <nav>
-      <img src="/logo.png" height="40rem" alt="" />
+      <Link to ="/">
+        <img src="/logo.png" height="40rem" alt="" />
+      </Link>
       <div className="nav-links">
         {/* <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/products">Products</Link>
         <Link to="/partners">Partners</Link> */}
         <button className="start-button">
-          About Us
+          Pricing
         </button>
       </div>
     </nav>
---
+++ frontend/my-app/src/components/Stats.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Stats.css'
+
+export default function Stats() {
+  return (
+    <div>
+      <h1>Welcome to the New Page!</h1>
+      <p>This is where you can start your journey.</p>
+    </div>
+  );
+}
