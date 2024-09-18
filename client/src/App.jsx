e51685035e545454e4a702641b15507e3d723725
Ashish Lal <ashishlal2003@gmail.com>
2024-09-18 12:39:59 +0530

    base second page test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Stats from './pages/Stats';
 
 export default function App() {
   return (
     <div className='bg-black px-36 p-5'>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/stats" element={<Stats />} />
+      </Routes>
     </div>
   )
 }
---
+++ client/src/pages/Stats.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Stats() {
+  return (
+    <div className='text-white'>
+      This is second page
+    </div>
+  )
+}
+
+export default Stats
